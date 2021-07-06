0b6d15b3ff89fe3582c8a88add552deb1673ba50
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-07-06 18:56:26 +0100

    scrap web drama

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+const userModel = require("./user.controler");
+const Postodel = require("./post.controller");
+
+const authModel = require("./auth.controler");
+
+module.exports = { userModel, Postodel, authModel };
